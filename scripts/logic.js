e6b8d7a31b0183280b503419d39efe2ebc4b8e95
Michael McDonald <michael.mcdonald@10-248-240-248.wireless.oregonstate.edu>
2018-09-27 15:38:31 -0700

    DRAG AND DROP WOOOO

--- scripts/logic.js
+++ scripts/logic.js
@@ -3,10 +3,15 @@ var fuelLimit = 10;
 var power = 0;
 var powerGen = 1;
 var fuelBurn = 1;
+var generatorTick = 1000;
+var clickValue = 1;
 
-function fuelClick(number) {
+var fuelInv = document.getElementById("fuel-inventory");
+var fuelSlot = document.getElementById("fuel-slot");
+
+function fuelClick(clickValue) {
     if (fuel < fuelLimit) {
-        fuel = fuel + number;
+        fuel = fuel + clickValue;
     }
     document.getElementById("fuel").innerHTML = fuel;
 }
@@ -20,8 +25,46 @@ function generate() {
     document.getElementById("fuel").innerHTML = fuel;
 };
 
-window.setInterval(function(){
-    
+window.setInterval(function() {
+
     generate();
-    
-}, 1000);
+
+}, generatorTick);
+
+//Drag and drop stuff below
+
+var drake = dragula({
+    isContainer: function(el) {
+        return el.classList.contains('drag-container');
+    },
+    copy: function(el, source) {
+        return source === document.getElementById("fuel-inventory")
+    },
+    accepts: function(el, target) {
+        return target !== document.getElementById("fuel-inventory")
+    },
+    moves: function(el) {
+        if (el.classList.contains('no-drag')) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+});
+
+drake.on('drop', function (el, target) {
+  console.log('Fuel placed in reactor.');
+  lockFuel();
+});
+
+
+function lockFuel() {
+var fuel = document.getElementsByClassName("fuel");
+for(var i = 0; i < fuel.length; i++)
+{
+    fuel[i].classList.add('no-drag');
+}
+}
+
+
+
